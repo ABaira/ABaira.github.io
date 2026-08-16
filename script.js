83dec54b111ef898ab84919847ed373254521b59
Adrian B <abaira1@nait.ca>
2026-08-16 16:52:39 -0600

    fix

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function sendEmail(email, name, message) {
         name: name,
         message: message
     };
-    console.log('Sending email with parameters:');
+    console.log('Sending email with parameters:', parameters);
     //Send the email using EmailJS
     //service ID, template ID, and parameters form Email.js sent to Icloud email addressW
     emailjs.send('service_68kutn9', 'template_02ia3ed', parameters);
